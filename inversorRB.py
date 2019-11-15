778e65e642e1f5fea576c66cf95a51dd456f22f0
ArmandoA <armando.arroyo.t@gmail.com>
2019-11-15 00:06:30 -0600

    Mi codigo hace algo. No estoy seguro si es correcto, pero hace algo.

---
+++ inversorRB.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from __future__ import division 
+from PIL import Image 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import colorsys
+
+#matplotlib inline
+
+imagen = Image.open("./i-see-no-god.png")
+RGBarr = np.asarray(imagen)
+#imagen.show()
+
+#print imagen.size, imagen.mode, imagen.format
+
+#imagengris = imagen.convert('L')
+
+#print imagengris.mode
+
+#imagengris.save('i-see-no-god-gris.tif')
+
+xs, ys = imagen.size
+
+newRGB = np.tile(0,(xs, ys, 3))
+
+red = RGBarr[..., 0]
+blue = RGBarr[..., 2]
+green = RGBarr[...,1]
+
+newRGB[..., 0] = blue 
+newRGB[..., 2] = red
+newRGB[..., 1] = green
+
+nueva = Image.fromarray(newRGB, 'RGB')
+
+nueva.save('nueva.png')
+
+nueva.show() 
+
+
